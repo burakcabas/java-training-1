8d0da220297f847d8f9618d4640436234679a84c
Burak Cabas <burakcabas3@gmail.com>
2024-12-13 17:40:50 +0300

    recap

---
+++ src/main/java/recap/one.java
@@ -0,0 +1,34 @@
+package recap;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class one {
+
+    public static void main(String[] args){
+
+        System.out.println("Two times two is: " + 2*2);
+
+        //this is a single-line comment
+
+        /*
+        this
+        is
+        a
+        multi-line
+        comment
+         */
+
+        String text = "Comrade";
+        float floatNumber = 3.32f; // f koymayınca hata veriyor
+        double doubleNumber = 3.32d; // d koymasan da olur
+        int intNumber = 56;
+        char singleLetter = 'b';
+        boolean isItTrue = false;
+
+
+
+    }
+
+}
