f544e1282921911944370be59442da30ff6116bf
Burak Cabas <burakcabas3@gmail.com>
2024-12-13 19:01:41 +0300

    recap 2

--- src/main/java/recap/one.java
+++ src/main/java/recap/one.java
@@ -8,6 +8,8 @@ public class one {
 
     public static void main(String[] args){
 
+        // Java is case-sensitive
+
         System.out.println("Two times two is: " + 2*2);
 
         //this is a single-line comment
@@ -21,13 +23,28 @@ public class one {
          */
 
         String text = "Comrade";
+        byte como = 3; // çok küçük aralıkta sayılar, -128 +127
         float floatNumber = 3.32f; // f koymayınca hata veriyor
         double doubleNumber = 3.32d; // d koymasan da olur
-        int intNumber = 56;
+        int intNumber = 56, crazyNumber = 49, lastButNotLeast = 100;
+        /*
+        int a = b = c = 19;
+        the code above will get us an error, gotta do it like line 26
+        */
         char singleLetter = 'b';
         boolean isItTrue = false;
 
+        System.out.println(intNumber + " " + crazyNumber + " " + lastButNotLeast);
 
+        int a = 9;
+        double b = a;
+
+        System.out.println(a + " " + b);
+
+        double d = 10.86;
+        int i = (int) d; // (int) kısmını yazmazsak hata alıyoruz
+
+        System.out.println(d + " " + i);
 
     }
 
